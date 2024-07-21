05290cab6b11d3dc683960c198452af05fd92c5d
Trang <pctrangmeo161@gmail.com>
2024-07-22 01:26:34 +0900

    added data, fixed styles

--- main.js
+++ main.js
@@ -1,8 +1,152 @@
+const jsonData = [
+  {
+    "studentNumb": "A21DC048",
+    "name": "折原リラ",
+    isMale: false, 
+    "titleJa": "アクタープロフィール制作プロジェクト",
+    "summaryJa": "私がインターン先の制作会社でマネージャーをさせていただいている役者さんのプロフィールサイトを作成します！かつ、様々なコンテンツ作成や各SNSを通したプロモーションを通してWEBサイトへのアプローチを考えます。",
+    "nameEn": "Lila ORIHARA",
+    "titleEn": "Actor's Official Profile Website Project",
+    "summaryEn": "I would like to make a web site about an actor's official profile site which I'm managing now at my creating company where I'm working at. I'll also make some video contents and operate some SNS to think about the promotion strategy.",
+    "image": "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpiha97iq3oVWZxzYmZX2Pf92V7PrIh5vlEpdHHK9bAAeTemi0jI27jSoJwc_vvZRgi4YoEity7LMmcdnyYb12ie1ayykW0Mq5qr0=w1920-h919-rw-v1"
+  },
+  {
+    "studentNumb": "A21DC069",
+    "name": "木村美希",
+    isMale: false, 
+    "titleJa": "あそびバ！",
+    "summaryJa": "手遊びや鬼ごっこなどの昔の遊びを紹介するサイトです。",
+    "nameEn": "Miki KIMURA",
+    "titleEn": "Asobiba!",
+    "summaryEn": "Asobiba! is a website that introduces traditional games, such as hand games and tag games.",
+    "image": ""
+  },
+  {
+    "studentNumb": "A21DC097",
+    "name": "坂入俊輔",
+    isMale: true, 
+    "titleJa": "ITで日常をアップデートするデジタル手帳サービス",
+    "summaryJa": "『スケジュール』を軸にして日常のちょっとした手間を省きます」をコンセプトに、予定に紐づく、起床時間、買い物の予定と買ったもの（家計簿）、タスクや買い物のメモとスケジュールというような関連情報を一つのアプリで処理、完結させ、外部サービス組み合わせからの脱却を目指します。",
+    "nameEn": "Shunsuke SAKAIRI",
+    "titleEn": "Digital Organizer Service to Update Daily Activites with IT",
+    "summaryEn": "This service processes and integrates all daily activites such as wake-up time, shopping list and purchases (household account book), tasks, notes and events, which are all personalized to the user's schedule, in just one application. With the concept of 'saving time and effort in daily life by focusing on the schedule,' this application aims to break away from the hassle of combining multiple external services.",
+    "image": "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihYAy2RNvcG2xZI82oLl6c9aKKZXP-wle-PbbW9QMmLE2_lkBAAfv0soq0yoA1M-H36fjqAnw0BDt6Vp_NXlgW0d-Hm4TcjpOA=w958-h918-rw-v1"
+  },
+  {
+    "studentNumb": "A21DC101",
+    "name": "佐藤翔英",
+    isMale: true, 
+    "titleJa": "格闘ゲームのコミュニティを活性化させるWebアプリケーションの制作",
+    "summaryJa": "格闘ゲームプレイヤーのスキルレベルに応じたマッチングが行えるシステムを制作し、初心者から中上級者までのプレイヤーが継続して楽しめる環境を提供することで、格闘ゲームコミュニティの活性化を目指す。",
+    "nameEn": "Shoei SATO",
+    "titleEn": "Web Application Development to Stimulate the Fighting Game Community",
+    "summaryEn": "By creating a system that allows fighting game players to be matched according to their skill level, the project aims to revitalize the fighting game community by providing an environment where players of all skill levels, from beginners to intermediate to advanced, can continue to enjoy their games.",
+    "image": ""
+  },
+  {
+    "studentNumb": "A21DC148",
+    "name": "ニュースバムシオン",
+    isMale: true, 
+    "titleJa": "Webサイトで描くMY取扱説明書",
+    "summaryJa": "自分の今までの経歴やこれからの活動、個人のファッションブランドを含めたセルフブランディングです。",
+    "nameEn": "Sion NUSBAUM",
+    "titleEn": "MY Instruction Manual, Portrayed on Website",
+    "summaryEn": "A self-branding project that includes my past and future activities, and my personal fashion brand.",
+    "image": "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihYyRkSIypTVQC3SD4WFhClFqSxlT4KCQ6kioAdl78f_aXbGA1dRykpYTKeK37_ICjZrZaSB8dXq8f1XI1CUOsCGTB9n8ggEsb8=w958-h918-rw-v1"
+  },
+  {
+    "studentNumb": "A21DC155",
+    "name": "林詩音",
+    isMale: false, 
+    "titleJa": "Strawberry Week",
+    "summaryJa": "医学的に女性の身体に月に1度訪れる『生理』についてあなたはどれくらい知っていますか？また生理中の症状がどのような感じか知っていますか？身近な存在なのに意外と知らない生理について知ることができる教材サイト『Strawberry Week』を制作しています。ただ生理について勉強するだけでなく、人それぞれ違う生理中の症状について、パートナーや家族、気心知れた友達と共有することが出来るので、生理でつらい時に支え合うことを可能にします。",
+    "nameEn": "Kotone HAYASHI",
+    "titleEn": "Strawberry Week",
+    "summaryEn": "How much do you know about menstruation, which medically occurs once a month in a woman's body? Do you know what the symptoms of menstruation are? I am creating 'Strawberry Week,' a website that provides educational contents to help you learn about menstruation, which you may be familiar with but may not know much about. Not only can you learn about menstruation, but you can also share the different symptoms of menstruation with your partner, family, and friends to support each other during your menstruation period.",
+    "image": "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihZgbEsqVeKIOjuIfisMhSG2t-3vpjN_af84OkVtaR8Oxv3k8br_O0XI7HiH7DJJvyjJpiNz5A8RywtyUHpNg2hs-1SX5dpUFYk=w958-h918-rw-v1"
+  },
+  {
+    "studentNumb": "A21DC217",
+    "name": "齋藤款",
+    isMale: true, 
+    "titleJa": "議論を楽しくする Web アプリ「Openio」",
+    "summaryJa": "人と人による意思決定や、その過程はとても複雑で、絶対有効だ！というUXは存在しません。その中で、このアプリの開発を通して、議論が嫌いな人も、議論が好きな人も、議論を必要と感じる全ての人に、その楽しさや面白さを届けます！",
+    "nameEn": "Shin SAITO",
+    "titleEn": "Openio: The Web App that Makes Discussions Fun",
+    "summaryEn": "Decision making by people and the process of decision making are very complex, and there is no UX that is absolutely valid. In this context, through the development of this application, we will bring the fun and excitement of discussion to everyone who feels the need to discuss, whether they dislike discussion or love it!",
+    "image": "img/openio.svg"
+  },
+  {
+    "studentNumb": "A21DC530",
+    "name": "NGUYEN MINH TRANG",
+    isMale: false, 
+    "titleJa": "たびぷら",
+    "summaryJa": "たびぷらは、AIを活用した旅行計画アプリで、あなたの旅行体験を楽しく簡単にします。希望や条件を入力するだけで、たびぷらが自動的にカスタマイズされた旅程を作成します。時間を節約し、ストレスを軽減し、フライト、宿泊施設、アクティビティ、予算管理の推奨を含む、あなただけの特別な旅行を楽しみましょう。",
+    "nameEn": "Minh Trang NGUYEN",
+    "titleEn": "Tabipura - Your Personalized Travel Planner",
+    "summaryEn": "Tabipura is an AI-powered travel planning application designed to make your travel experience effortless and enjoyable. Simply input your preferences and conditions, and Tabipura will automatically generate a customized itinerary for you. Save time, reduce stress, and enjoy a tailored travel experience with recommendations for flights, accommodations, activities, and budget management.",
+    "image": ""
+  },
+  {
+    "studentNumb": "A21DC535",
+    "name": "ジョミンジュ",
+    isMale: false, 
+    "titleJa": "Fizzle Out（社会人新人のための金融教育ゲームプロジェクト）",
+    "summaryJa": "住民税申告期間を逃して国民健康保険料関連問題を体験した経験を土台に、20代社会人新人を対象にした金融教育推理ゲームの企画。 FigmaとAi、Psなどを活用し、ゲームに必要なキャラクターデザイン及び手がかりアセットデザイン、背景デザイン、UIデザインなどを担当。",
+    "nameEn": "Minju JO",
+    "titleEn": "Fizzle Out (Financial Education Game for New Professionals)",
+    "summaryEn": "Based on the experience of missing the resident tax report period and experiencing problems related to national health insurance premiums, I'm planning a financial education reasoning game for newcomers in their 20s. Using Figma, Ai, Ps, etc. I'm in charge of character design and clue asset design, background design, UI design, etc. necessary for the game.",
+    "image": "img/fizzle.jpg"
+  },
+  {
+    "studentNumb": "A21DC553",
+    "name": "TRAN MINH TRI",
+    isMale: true, 
+    "titleJa": "「ランニングへようこそ」プロジェクト",
+    "summaryJa": "私たちは、誰もが参加できる新しいスポーツとして、ランニングを紹介したいと考えています。ランニングは単に健康を向上させるだけでなく、新しい価値観を生み出し、社会の発展にも貢献します。 このウェブサイトは、ランニングの効果とランニングが私たちの身体に与える影響について、人々がよりよく理解できるように作成します。",
+    "nameEn": "Minh Tri TRAN",
+    "titleEn": "The \"Welcome to Running\" Project",
+    "summaryEn": "This website introduces running as a new sport, in which everyone can participate. Running not only improves our health, but also creates new values and contributes to the development of society. We want to create a better way for people to understand the benefits of running and the impact running has on our bodies.",
+    "image": ""
+  },
+  {
+    "studentNumb": "A21DC572",
+    "name": "MAHARANI DANIELLA",
+    isMale: false, 
+    "titleJa": "インドネシアの伝統衣装を紹介する「バティックナビ」",
+    "summaryJa": "バティックナビは、インドネシアの各地域の伝統衣装を紹介するWebサイトです。インドネシアの34地域にわたる衣装のプロトタイプを紹介し、それぞれの地域ごとに男性用と女性用の衣装を合わせて68着のイメージを展示します。",
+    "nameEn": "Daniella MAHARANI",
+    "titleEn": "Batik Navi - Introducing Traditional Indonesian Attires",
+    "summaryEn": "Batik Navi is a website that introduces traditional costumes from various regions of Indonesia. Prototypes of costumes spanning 34 regions of Indonesia are presented, with 68 images of men's and women's costumes for each region.",
+    "image": "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihZ_bfkeeMeTe200pJeJdVmhgqb9N_g05UfFehNZ0NJ_Cb6dMXO0B8zKIq2XxbbY3c_Zjff3WWuE6vMslrtCU-ZrIheCY_xO9TQ=w958-h918-rw-v1"
+  }
+]
+
 $(document).ready(function() {
-  function generateImages(container, count) {
-    for (let i = 0; i < count; i++) {
-        $(container).append('<li class="splide__slide"  data-bs-toggle="modal"><img src="img/white.png" alt="Image ' + (i + 1) + '" class="dynamic-image"></li>');
-    }
+  function generateImages(container, json, isStudent) {
+    json.forEach((item, index) => {
+        let imageUrl = !isStudent? item.image ? item.image : "img/white.png" : "";
+        let altText = !isStudent? item.image ? "Image " + (index + 1) : "Coming soon": item.name;
+        
+        !isStudent ? $(container).append(
+          '<li class="splide__slide" data-bs-toggle="modal" ' +
+          'data-title="' + item.titleJa + '" ' +
+          'data-name="' + item.name + '" ' +
+          'data-summary="' + item.summaryJa + '" ' +
+          'data-image="' + imageUrl + '">' +
+          '<div class="image-wrapper">' +
+          '<img src="' + imageUrl + '" alt="' + altText + '" class="dynamic-image">' +
+          (item.image ? '' : '<span class="coming-soon-text">Coming soon</span>') +
+          '</div>' +
+          '</li>'
+      ) : $(container).append(
+        `<li class="splide__slide" >
+          <div class="student-wrapper">
+          <div class="student-image"><img src="${item.isMale? 'img/user-male.svg': 'img/user-female.svg'}" alt="${item.name}"</div>
+          </div>
+          <span>${item.name}</span>
+        </li>`);
+    });
 }
 
 function initializeSplide(selector) {
@@ -30,17 +174,25 @@ function initializeSplide(selector) {
 }
 
  // Generate images for both carousels
- generateImages('#splide-student .splide__list', 9);
- generateImages('#st_pic .splide__list', 9);
+ generateImages('#splide-student .splide__list', jsonData, true);
+ generateImages('#st_pic .splide__list', jsonData, false);
 
  // Initialize Splide for both carousels
  initializeSplide('#splide-student');
  initializeSplide('#st_pic');
 
- // Event listener for image click to show modal
- $('.splide').on('click', '.dynamic-image', function() {
-  var src = $(this).attr('src');
+// Event listener for image click to show modal with detailed information
+$('#st_pic').on('click', '.splide__slide', function() {
+  var title = $(this).data('title');
+  var name = $(this).data('name');
+  var summary = $(this).data('summary');
+  var src = $(this).data('image');
+
   $('#modalImage').attr('src', src);
+  $('#modalName').text("by " + name);
+  $('#modalTitle').text(title);
+  $('#modalSummary').text(summary);
+
   var modal = new bootstrap.Modal(document.getElementById('imageModal'));
   modal.show();
 });
@@ -69,13 +221,13 @@ $('.title').on('click', function() {//タイトル要素をクリックしたら
     }
   });
   
-  //ページが読み込まれた際にopenクラスをつけ、openがついていたら開く動作※不必要なら下記全て削除
-  $(window).on('load', function(){
-    $('.accordion-area li:first-of-type section').addClass("open"); //accordion-areaのはじめのliにあるsectionにopenクラスを追加
-    $(".open").each(function(index, element){ //openクラスを取得
-      var Title =$(element).children('.title'); //openクラスの子要素のtitleクラスを取得
-      $(Title).addClass('close');       //タイトルにクラス名closeを付与し
-      var Box =$(element).children('.box'); //openクラスの子要素boxクラスを取得
-      $(Box).slideDown(500);          //アコーディオンを開く
-    });
-  });
+//   //ページが読み込まれた際にopenクラスをつけ、openがついていたら開く動作※不必要なら下記全て削除
+//   $(window).on('load', function(){
+//     $('.accordion-area li:first-of-type section').addClass("open"); //accordion-areaのはじめのliにあるsectionにopenクラスを追加
+//     $(".open").each(function(index, element){ //openクラスを取得
+//       var Title =$(element).children('.title'); //openクラスの子要素のtitleクラスを取得
+//       $(Title).addClass('close');       //タイトルにクラス名closeを付与し
+//       var Box =$(element).children('.box'); //openクラスの子要素boxクラスを取得
+//       $(Box).slideDown(500);          //アコーディオンを開く
+//     });
+//   });
